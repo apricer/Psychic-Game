46eb8d933960cb0e27cf386d74b8e746d8790e55
Anthony Pricer <anthony.pricer@outlook.com>
2019-09-28 22:43:32 -0500

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,11 @@
 
+// Alert pop-up telling user they have 10 guesses, A - Z
+alert("You have 10 guesses. Guess A - Z");
+
+// Create for loop for 10 guesses
+for(i=0; i < 10; i++){
+    console.log(i + 1);
+}
 
 // Create an array of the letters of the alphabet
 var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -8,4 +15,25 @@ document.onkeyup = function() {
     var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
     console.log(userguess);
+
+
+    // Computer generates a letter at random
+    var computerGuess = options[Math.floor(Math.random()*options.length)];
+
+    console.log(computerGuess);
+
+    // Counting number of guesses
+
+    // Counting number of wins
+
+    // Counting number of losses
+
+    // Number of guesses left
+
+    // Letters guessed so far
+
+    // Increasing win counter
+
+    // Increasing loss counter
+
 }
