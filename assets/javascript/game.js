f80b654d117bd32a7bf10b5a5468a6f443aa22c3
Anthony Pricer <anthony.pricer@outlook.com>
2019-09-30 00:21:40 -0500

    Psychic Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,55 @@
 
-// Alert pop-up telling user they have 10 guesses, A - Z
 alert("You have 10 guesses. Guess A - Z");
 
-// Create for loop for 10 guesses
-for(i=0; i < 10; i++){
-    console.log(i + 1);
-}
-
-// Create an array of the letters of the alphabet
 var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var wins = 0;
+var losses = 0;
+var remaining = 9;
+var guesses = 9;
+var computerLetter;
+var yourGuessesSoFar = [];
 
-// Create code so it is noted and logged when user presses a button in the array
-document.onkeyup = function() {
-    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    console.log(userguess);
-
-
-    // Computer generates a letter at random
-    var computerGuess = options[Math.floor(Math.random()*options.length)];
-
+var letterGenerate = function () {
+    var computerLetter = options[Math.floor(Math.random()*options.length)];
     console.log(computerGuess);
-
-    // Counting number of guesses
-
-    // Counting number of wins
-
-    // Counting number of losses
-
-    // Number of guesses left
-
-    // Letters guessed so far
-
-    // Increasing win counter
-
-    // Increasing loss counter
-
 }
+
+var currentGuesses = function() {
+    document.getElementById("guesses").innerHTML = "Your Guesses so Far: " + yourGuessesSoFar.join(",");
+}
+
+var guessesRemaining = function() {
+    document.getElementById("remaining").innerHTML = "Guesses Remaining: " + remaining;
+}
+
+var startNewGame = function() {
+    lettersGuessed = [];
+    remaining = 9;
+    letterGenerate();
+    currentGuesses();
+    guessesRemaining();
+}
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    remaining--;
+    yourGuessesSoFar.push(userGuess);
+    var currentGuesses();
+    guessesRemaining();
+    if (left > 0) {
+        if (userGuess === computerLetter) {
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins:" + wins;
+            startNewGame();
+        }
+    
+    }
+
+    else if (left === 0) {
+        losses ++;
+        document.getElementById("losses").innerHTML = "Losses: " + losses;
+        startNewGame();
+    }
+}
+    
